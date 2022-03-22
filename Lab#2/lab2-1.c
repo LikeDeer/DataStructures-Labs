f3913dd5a3c90ed21c8cfb90ce0001ca6e4696e2
LikeDeer <stkjas4657@naver.com>
2022-03-22 11:27:02 +0900

    comment on lab2-1

--- Lab#2/lab2-1.c
+++ Lab#2/lab2-1.c
@@ -8,25 +8,25 @@ int main() {
 
     printf("----- [정준호]  [2020069046] -----\n");
 
-    printf("Size of char : %ld byte\n", sizeof(charType));      // 
-    printf("Size of int: %ld bytes\n", sizeof(integerType));
-    printf("Size of float: %ld bytes\n", sizeof(floatType));
-    printf("Size of double: %ld bytes\n", sizeof(doubleType));
+    printf("Size of char : %ld byte\n", sizeof(charType));          // char형 변수의 크기 (1 byte)
+    printf("Size of int: %ld bytes\n", sizeof(integerType));        // int형 변수의 크기 (4 byte)
+    printf("Size of float: %ld bytes\n", sizeof(floatType));        // float형 변수의 크기 (4 byte)
+    printf("Size of double: %ld bytes\n", sizeof(doubleType));      // double형 변수의 크기 (8 byte)
 
 
     printf("----------------------------------\n");
 
-    printf("Size of char: %ld byte\n", sizeof(char));
-    printf("Size of int: %ld bytes\n", sizeof(int));
-    printf("Size of float: %ld bytes\n", sizeof(float));
-    printf("Size of double: %ld bytes\n", sizeof(double));
+    printf("Size of char: %ld byte\n", sizeof(char));               // char 자료형의 크기 (1 byte)
+    printf("Size of int: %ld bytes\n", sizeof(int));                // int 자료형의 크기 (4 byte)
+    printf("Size of float: %ld bytes\n", sizeof(float));            // float 자료형의 크기 (4 byte)
+    printf("Size of double: %ld bytes\n", sizeof(double));          // double 자료형의 크기 (8 byte)
 
     printf("----------------------------------\n");
 
-    printf("Size of char*: %ld byte\n", sizeof(char*));
-    printf("Size of int*: %ld bytes\n", sizeof(int*));
-    printf("Size of float*: %ld bytes\n", sizeof(float*));
-    printf("Size of double*: %ld bytes\n", sizeof(double*));
+    printf("Size of char*: %ld byte\n", sizeof(char*));             // char* 포인터의 크기. 64bit 리눅스(운영체제) 이므로 8 byte
+    printf("Size of int*: %ld bytes\n", sizeof(int*));              // int* 포인터의 크기. 64bit 리눅스(운영체제) 이므로 8 byte
+    printf("Size of float*: %ld bytes\n", sizeof(float*));          // float* 포인터의 크기. 64bit 리눅스(운영체제) 이므로 8 byte
+    printf("Size of double*: %ld bytes\n", sizeof(double*));        // double* 포인터의 크기. 64bit 리눅스(운영체제) 이므로 8 byte
 
     return 0;
 }
