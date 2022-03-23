470c032c406fb9bb36d794ffa727726dfcadca79
LikeDeer <stkjas4657@naver.com>
2022-03-23 11:39:01 +0900

    p2-2.c

--- Lab#3/p2-2.c
+++ Lab#3/p2-2.c
@@ -8,11 +8,19 @@ int main() {
     printf("one         = %p\n", one);
     printf("&one        = %p\n", &one);
     printf("&one[0]     = %p\n", &one[0]);
-    printf"\n");
+    printf("\n");
 
     print1(&one[0], 5);
 
     return 0;
 }
 
-void 
+void printf1(int *ptr, int rows) {
+    /* print out a one-dimensional array using a pointer */
+
+    int i;
+    printf("Address \t Contents\n");
+    for (i = 0; i < rows; i++)
+        printf("%p \t %5d\n", ptr + i, *(ptr + i));
+    printf("\n");
+}
