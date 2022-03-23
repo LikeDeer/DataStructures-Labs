94c0b099aa1a475ff50284a07b514300a033ae3d
LikeDeer <stkjas4657@naver.com>
2022-03-23 11:17:12 +0900

    ap1.c done coding

--- Lab#3/ap1.c
+++ Lab#3/ap1.c
@@ -12,6 +12,8 @@ void main() {
 
     *plist[0] = 200;
 
+    printf("    [----- 정준호 2020069046 -----]\n\n");
+
     printf("value of list[0]        = %d\n", list[0]);
     printf("address of list[0]      = %p\n", &list[0]);
     printf("value of list           = %p\n", list);
@@ -24,5 +26,15 @@ void main() {
     printf("address of list+1       = %p\n", list+1);
 
     printf("---------------------------------------------\n\n");
-    printf("value of *plist]0\      = %d\n", *plist[0]);
+    printf("value of *plist[0]      = %d\n", *plist[0]);
+    printf("&plist[0]               = %p\n", &plist[0]);
+    printf("&plist                  = %p\n", &plist);
+    printf("plist                   = %p\n", plist);
+    printf("plist[0]                = %p \n", plist[0]);
+    printf("plist[1]                = %p \n", plist[1]);
+    printf("plist[2]                = %p \n", plist[2]);
+    printf("plist[3]                = %p \n", plist[3]);
+    printf("plist[4]                = %p \n", plist[4]);
+
+    free(plist[0]);
 }
